bb85f63495fbdf5ef7a187fb90c9612f7d85c5ed
Riyadh <albarrriyadh1112@gmail.com>
2023-02-23 11:29:25 +0800

    Init, register, login, & home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+import Head from "next/head";
+import Script from "next/script";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+          crossorigin="anonymous"
+        ></link>
+      </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"
+      />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
